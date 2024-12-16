c7798c41d59407d1962e67b6e482e5c10dc411e4
Morgane Ortic <morgane.ortic@proton.me>
2024-12-16 15:41:36 +0100

    Set up env

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-import json
 import simplematrixbotlib as botlib
 import pandas as pd
+import os
+from dotenv import load_dotenv
 from import_weather import check_temp
 
 creds_file = 'creds.json'
 
-# def import_creds():
-#     '''Import credentials from a json file'''
-#     with open(creds_file, 'r') as file:
-#         creds_data = json.load(file)
-#     # unpacking each credential from the list
-#     if isinstance(creds_data, list) and len(creds_data) == 3:
-#         homeserver, username, password = creds_data
-#     else:
-#         raise ValueError("Unable to import credentials")
-    
-#     return homeserver, username, password
+load_dotenv()
 
-# # Call import_creds() function and unpack the returned values
-# homeserver, username, password = import_creds()
-
-# creds = botlib.Creds(homeserver, username, password)
 
 class FrostBot():
     def __init__(self, timezone):

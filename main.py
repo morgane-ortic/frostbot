afdcdc2e3cbf8fb8ff9134ff8bf501ae637b92bf
Morgane Ortic <morgane.ortic@proton.me>
2024-12-16 15:20:34 +0100

    set up basic frost checker in main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import json
 import simplematrixbotlib as botlib
-
+import pandas as pd
 from import_weather import check_temp
 
 creds_file = 'creds.json'
-timezone = "Europe/Berlin"
 
 # def import_creds():
 #     '''Import credentials from a json file'''
@@ -23,6 +22,38 @@ timezone = "Europe/Berlin"
 
 # creds = botlib.Creds(homeserver, username, password)
 
-hourly_dataframe = check_temp(timezone)
+class FrostBot():
+    def __init__(self, timezone):
+        self.timezone = timezone
 
-print(hourly_dataframe)
+
+    def format_neg_temp(self, records):
+        for record in records.values():
+            record['date'] = pd.to_datetime(record['date']).strftime('%d-%m-%Y %H:%M')
+            record['temperature_2m'] = round(record['temperature_2m'], 1)
+        return records
+
+
+    def check_frost(self):
+        # Get the DataFrame and reset the index to include it as a column
+        self.hourly_temp = check_temp(self.timezone).to_dict(orient='index')
+        
+        below_zero = {index: record for index, record in self.hourly_temp.items() if record['temperature_2m'] < 8}
+
+        if below_zero:
+            print("Temperatures below 0 detected:")
+            # Format the records
+            formatted_below_zero = self.format_neg_temp(below_zero)
+            for index, record in formatted_below_zero.items():
+                print(f"Date: {record['date']}, Temperature: {record['temperature_2m']}")
+        else:
+            print("No temperatures below 0 detected.")
+
+
+def main():
+    frost_bot = FrostBot("Europe/Berlin")
+    frost_bot.check_frost()
+
+
+if __name__ == "__main__":
+    main()
